f296ee3337fc0f1ad6d1b78aed3f3a83660f4708
Balakrishna Sairam Pratapa <19125760073@datascience.manipal.edu>
2020-07-29 00:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
-
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 import pickle
 
 # load the model from disk
