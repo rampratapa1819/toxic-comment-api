9c6edcba0958ca47892da515243e4f04ef23571f
Balakrishna Sairam Pratapa <19125760073@datascience.manipal.edu>
2020-07-31 15:44:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def predict():
 		message = request.form['message']
 		data = [message]
 		seq=tokenizer.texts_to_sequences(data)
-		seq=seq.toarray()
     		padded=pad_sequences(seq,maxlen=100)
     		pred=save.predict(padded)
     		pred = np.round(pred)
