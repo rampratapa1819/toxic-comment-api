7b4293448a2b96235b3a730ed166b4723a90ccb9
Balakrishna Sairam Pratapa <19125760073@datascience.manipal.edu>
2020-07-31 14:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,48 +20,17 @@ def predict():
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
-        	stopwords_ = ["s","i","me",'my','myself','we','our','ours','a','the','your','ourselves','you',"you're","you've","you'll","you'd",'your',
-			      'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', "she","she's", 'her', 'hers', 'herself', 'it', "it's",'its', 'itself',
-			      'they', 'them', 'their','theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll",'these','those','am','is','are',
-			      'was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as',
-			      'until','while','of','at','by','for','with','about','against', 'between','into','through','during','before','after','above','below','to',
-			      'from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all',
-			      'any','both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very',
-			      's','t', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't",
-			      'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",'ma','mightn',
-			      "mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',
-			      "wouldn't",'best','different',"won't","couldn't","mustn't","didn't","aww","hey","man","women"]
-
-        	custom_stop = ["adv", "fundamentals", "course", "advance", "beginner", "beginners","nbsp", "help", "would", "could", "courses","also","get",
-			       "give","class","much",'copy0','copy1','nbsp','course','copy','best','different']
-
-        	stopwords_= stopwords_ + custom_stop
-
-
-        	def  clean_text(sent):
-        	tokens = sent.lower().replace('[^a-z ]',' ').split()
-        	stemmed = [term for term in tokens if term not in stopwords_ and len(term) > 2] 
-        	res = " ".join(stemmed)
-        	return res
-    
-        	def output(txt_values):
-            	clean = [ ]
-            	for i in txt_values:
-			clean.append(clean_text(i))
-            	seq=tokenizer.texts_to_sequences(clean)
-            	padded=pad_sequences(seq,maxlen=100)
-            	pred=save.predict(padded)
-            	pred = np.round(pred)
-            	return pred
-		my_predictions=list(output(data))
+		seq=tokenizer.texts_to_sequences(clean)
+    		padded=pad_sequences(seq,maxlen=100)
+    		pred=save.predict(padded)
+    		pred = list(np.round(pred))
 		out=[]
-		for i in my_predictions:
+		for i in pred:
 			if i==0:
-				out.append('Comment is Non-Toxic')
+				out.append('Non-Toxic')
 			else:
-				out.append('Comment is Toxic')
-				
-		
+				out.append('Toxic')
+    
 	return render_template('result.html',prediction = out)
 
 
