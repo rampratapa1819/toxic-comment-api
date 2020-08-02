6e371b5f9972e211bf9db9c2ed42657985dc3970
Balakrishna Sairam Pratapa <19125760073@datascience.manipal.edu>
2020-08-03 00:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
+import pandas as pd
+import numpy as np
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -19,36 +20,6 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-#	df= pd.read_csv("spam.csv", encoding="latin-1")
-#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-#	# Features and Labels
-#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-#	X = df['message']
-#	y = df['label']
-#	
-#	# Extract Feature With CountVectorizer
-#	cv = CountVectorizer()
-#	X = cv.fit_transform(X) # Fit the Data
-#    
-#    pickle.dump(cv, open('tranform.pkl', 'wb'))
-#    
-#    
-#	from sklearn.model_selection import train_test_split
-#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-#	#Naive Bayes Classifier
-#	from sklearn.naive_bayes import MultinomialNB
-#
-#	clf = MultinomialNB()
-#	clf.fit(X_train,y_train)
-#	clf.score(X_test,y_test)
-#    filename = 'nlp_model.pkl'
-#    pickle.dump(clf, open(filename, 'wb'))
-    
-	#Alternative Usage of Saved Model
-	# joblib.dump(clf, 'NB_spam_model.pkl')
-	# NB_spam_model = open('NB_spam_model.pkl','rb')
-	# clf = joblib.load(NB_spam_model)
-
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
@@ -57,15 +28,12 @@ def predict():
 		predictions = np.round(predictions)
 		my_prediction=list(prediction)
     		output =[]
-    		for i in prediction:
+    		for i in my_prediction:
         		if i==0:
             			output.append('Not Toxic')
         		else:
             			output.append('Toxic')
 
 	return render_template('result.html',prediction = output)
-
-
-
 if __name__ == '__main__':
 	app.run(debug=True)
